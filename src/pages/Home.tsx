04a819b2e511544c125c98dc539d1627ac8aed9c
Matrox1821 <matiasgg1821@gmail.com>
2022-12-17 20:57:38 -0300

    home changes

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -5,16 +5,18 @@ export const Home = () => {
   return (
     <div className="home-container">
       <h1 className="title-name">Galarza Matías</h1>
-      <div className="me-image">
+      <div className="container-image">
         <p className="japanese-words">ガラルザマティアス</p>
         <img src={Matias} className="image" alt="Galarza Matias con lentes" />
         <p className="japanese-words">ウェブ開発者</p>
       </div>
-      <h2 className="subtitle">Portafolio</h2>
-      <h3>Desarrollador Web</h3>
-      <div className="down-arrow">
-        <span className="line1"></span>
-        <span className="line2"></span>
+      <div className="subtitle">
+        <h2>Portafolio</h2>
+        <h3>Desarrollador Web</h3>
+        <div className="down-arrow">
+          <span className="line1"></span>
+          <span className="line2"></span>
+        </div>
       </div>
     </div>
   );
